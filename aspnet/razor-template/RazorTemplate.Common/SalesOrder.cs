e30a727cd0525986cb8854013dd9a77cba37aa6f
SamuGG <SamuGG@users.noreply.github.com>
2025-10-26 13:16:15 +0000

    feat: Razor template component

---
+++ aspnet/razor-template/RazorTemplate.Common/SalesOrder.cs
@@ -0,0 +1,6 @@
+﻿namespace RazorTemplate.Common;
+
+public sealed record SalesOrder(string OrderId, DateTime OrderDate, OrderCustomer Customer, IReadOnlyCollection<OrderLine> OrderLines);
+public sealed record OrderCustomer(string FirstName, string LastName);
+public sealed record OrderLine(OrderItem OrderItem, int Quantity, decimal Subtotal);
+public sealed record OrderItem(string Title, string Subheading, decimal UnitPrice);
