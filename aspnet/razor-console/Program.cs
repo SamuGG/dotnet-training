d44c97de41b0d22afcf5db434b6736404f186508
SamuGG <SamuGG@users.noreply.github.com>
2025-10-26 13:15:35 +0000

    feat: Render Razor from console

---
+++ aspnet/razor-console/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.HtmlRendering;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RazorConsole;
+
+var services = new ServiceCollection();
+services.AddLogging();
+
+IServiceProvider serviceProvider = services.BuildServiceProvider();
+var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+await using var htmlRenderer = new HtmlRenderer(serviceProvider, loggerFactory);
+
+string html = await htmlRenderer.Dispatcher.InvokeAsync(async () =>
+{
+    var componentParams = new Dictionary<string, object?>
+    {
+        { nameof(MessageComponent.MessageText), "Hello from the Razor component" }
+    };
+
+    var parameters = ParameterView.FromDictionary(componentParams);
+    HtmlRootComponent output = await htmlRenderer.RenderComponentAsync<MessageComponent>(parameters);
+
+    return output.ToHtmlString();
+});
+
+Console.WriteLine(html);
